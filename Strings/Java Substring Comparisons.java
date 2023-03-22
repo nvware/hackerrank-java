49d4367e77c69337cfa2ed2d2379149e61c93f16
nvware <h_valizadgan@yahoo.com>
2023-03-22 14:48:10 +0200

    Update Java Substring Comparisons.java

--- Strings/Java Substring Comparisons.java
+++ Strings/Java Substring Comparisons.java
@@ -5,10 +5,10 @@ public class Solution {
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
-        
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
+        
         java.util.List<String> sList=new java.util.ArrayList<>();
         for(int i=0;i<=s.length()-k;i++)
         {            
